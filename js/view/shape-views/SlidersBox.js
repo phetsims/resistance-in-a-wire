d667104379c2677aae0ccaecd9c4b67d42019921
samreid <reids@colorado.edu>
2014-01-03 19:07:35 -0700

    renamed sliderx => Slider to fix casing problem, see #47

--- js/view/shape-views/SlidersBox.js
+++ js/view/shape-views/SlidersBox.js
@@ -13,7 +13,7 @@ define( function( require ) {
   var Node = require( 'SCENERY/nodes/Node' );
   var inherit = require( 'PHET_CORE/inherit' );
   var WhiteBox = require( 'view/shape-views/slider-box-view/WhiteBox' );
-  var Slider = require( 'view/shape-views/slider-box-view/SlideRx' );
+  var Slider = require( 'view/shape-views/slider-box-view/Slider' );
   var CurrentResistanceView = require( "view/shape-views/slider-box-view/CurrentResistanceView" );
   var sliderImage = require( 'image!RESISTANCE_IN_A_WIRE/slider.png' );
   var Text = require( 'SCENERY/nodes/Text' );
