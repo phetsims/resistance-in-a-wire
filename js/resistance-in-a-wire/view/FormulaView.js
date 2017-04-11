a47ce61b4060f094bff873c7f2de6a1aedeaacb0
Martin Veillette <martin.veillette@gmail.com>
2017-04-10 23:20:47 -0400

    replace strings by the property variables

--- js/resistance-in-a-wire/view/FormulaView.js
+++ js/resistance-in-a-wire/view/FormulaView.js
@@ -46,28 +46,28 @@ define( function( require ) {
         scale: 3 / 2,
         x: 20,
         y: 0,
-        targetProperty: 'resistanceProperty',
+        targetProperty: model.resistanceProperty,
         color: '#ed1c24'
       },
       {
         label: resistivitySymbolString,
         x: 220,
         y: -90,
-        targetProperty: 'resistivityProperty',
+        targetProperty: model.resistivityProperty,
         color: ResistanceInAWireConstants.BLUE_COLOR
       },
       {
         label: lengthSymbolString,
         x: 320,
         y: -90,
-        targetProperty: 'lengthProperty',
+        targetProperty: model.lengthProperty,
         color: ResistanceInAWireConstants.BLUE_COLOR
       },
       {
         label: areaSymbolString,
         x: 280,
         y: 90,
-        targetProperty: 'areaProperty',
+        targetProperty: model.areaProperty,
         color: ResistanceInAWireConstants.BLUE_COLOR
       }
     ];
@@ -93,9 +93,9 @@ define( function( require ) {
       } );
       self.addChild( entry.view );
 
-      // TODO: use a callback rather than accessing property
-      entry.scale = entry.scale || 1 / model[ entry.targetProperty ].get();
-      model[ entry.targetProperty ].link( function updateProperty( val ) {
+
+      entry.scale = entry.scale || 1 / entry.targetProperty.value;
+      entry.targetProperty.link( function updateProperty( val ) {
         entry.view.matrix = Matrix3.identity();
         entry.view.scale( entry.scale * val + 0.125 );
         entry.view.centerX = entry.x;
