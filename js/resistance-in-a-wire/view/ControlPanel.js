4c1a7951b0d94263fa26054a8771236a7d950024
Michael Barlow <m.barlow12@gmail.com>
2017-06-28 10:17:13 -0600

    reinstate 100 step for area slider

--- js/resistance-in-a-wire/view/ControlPanel.js
+++ js/resistance-in-a-wire/view/ControlPanel.js
@@ -111,8 +111,7 @@ define( function( require ) {
       cmString + '<sup>2</sup>',
       tandem.createTandem( 'areaSlider' ),
       {
-        // keyboardStep: ( ResistanceInAWireConstants.AREA_RANGE.max - ResistanceInAWireConstants.AREA_RANGE.min ) / 100
-        keyboardStep: 0.2,
+        keyboardStep: ( ResistanceInAWireConstants.AREA_RANGE.max - ResistanceInAWireConstants.AREA_RANGE.min ) / 100
         modifiedKeyboardStep: 0.01
       }
     );
