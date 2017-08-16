1095c75f12c77251b5b734d44c9f7a2f5e4a38bb
zepumph <michael.kauzmann@colorado.edu>
2017-08-15 16:39:04 -0800

    remove unnecessary newlines or replace with <br>, https://github.com/phetsims/joist/issues/435

--- js/resistance-in-a-wire-main.js
+++ js/resistance-in-a-wire-main.js
@@ -27,7 +27,8 @@ define( function( require ) {
       team: 'Wendy Adams, Mindy Gratny, Ariel Paul',
       qualityAssurance: 'Steele Dalton, Alex Dornan, Bryce Griebenow, Ethan Johnson, Elise Morgan, ' +
                         'Oliver Orejola, Benjamin Roberts, Bryan Yoelin',
-      thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team\nto convert this simulation to HTML5.'
+      thanks: 'Thanks to Mobile Learner Labs for working with the PhET development team to convert this ' +
+              'simulation to HTML5.'
     }
   };
 
