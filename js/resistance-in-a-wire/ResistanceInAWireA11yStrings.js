f5bb0279430ac211a0024f20c465ef9272b3d2ec
Jesse Greenberg <jesse.greenberg@colorado.edu>
2017-10-06 17:44:39 -0600

    remove unused strings (verified not in latest publication), see #104

--- js/resistance-in-a-wire/ResistanceInAWireA11yStrings.js
+++ js/resistance-in-a-wire/ResistanceInAWireA11yStrings.js
@@ -10,21 +10,8 @@
 define( function( require ) {
   'use strict';
 
+  // modules
   var resistanceInAWire = require( 'RESISTANCE_IN_A_WIRE/resistanceInAWire' );
-
-  var hotKeysAndHelpString = require( 'string!RESISTANCE_IN_A_WIRE/hotKeysAndHelp' );
-  var orString = require( 'string!RESISTANCE_IN_A_WIRE/or' );
-  var shiftKeyString = require( 'string!RESISTANCE_IN_A_WIRE/shiftKey' );
-  var shiftTabKeyDescriptionString = require( 'string!RESISTANCE_IN_A_WIRE/shiftTabKeyDescription' );
-  var tabKeyDescriptionString = require( 'string!RESISTANCE_IN_A_WIRE/tabKeyDescription' );
-  var tabKeyString = require( 'string!RESISTANCE_IN_A_WIRE/tabKey' );
-  var arrowKeysAdjustSlidersString = require( 'string!RESISTANCE_IN_A_WIRE/arrowKeysAdjustSliders' );
-  var shiftArrowKeysSlidersString = require( 'string!RESISTANCE_IN_A_WIRE/shiftArrowKeysSliders' );
-  var pageUpPageDownSlidersString = require( 'string!RESISTANCE_IN_A_WIRE/pageUpPageDownSliders' );
-  var homeEndSlidersString = require( 'string!RESISTANCE_IN_A_WIRE/homeEndSliders' );
-  var escapeKeyDescriptionString = require( 'string!RESISTANCE_IN_A_WIRE/escapeKeyDescription' );
-  var sliderControlsString = require( 'string!RESISTANCE_IN_A_WIRE/sliderControls' ); 
-  var generalNavigationString = require( 'string!RESISTANCE_IN_A_WIRE/generalNavigation' ); 
 
   var ResistanceInAWireA11yStrings = {
     resistivityUnitsPatternString: '{{value}} ohm centimeters',
@@ -32,22 +19,7 @@ define( function( require ) {
     areaUnitsPatternString: '{{value}} centimeters squared',
     resistivitySliderLabelString: 'rho, Resistivity',
     lengthSliderLabelString: 'L, Length',
-    areaSliderLabelString: 'A, Area',
-
-    // keyboard help strings
-    hotKeysAndHelpString: hotKeysAndHelpString,
-    orString: orString,
-    shiftKeyString: shiftKeyString,
-    shiftTabKeyDescriptionString: shiftTabKeyDescriptionString,
-    tabKeyDescriptionString: tabKeyDescriptionString,
-    tabKeyString: tabKeyString,
-    arrowKeysAdjustSlidersString: arrowKeysAdjustSlidersString,
-    shiftArrowKeysSlidersString: shiftArrowKeysSlidersString, 
-    pageUpPageDownSlidersString: pageUpPageDownSlidersString, 
-    homeEndSlidersString: homeEndSlidersString, 
-    escapeKeyDescriptionString: escapeKeyDescriptionString,
-    sliderControlsString: sliderControlsString,
-    generalNavigationString: generalNavigationString
+    areaSliderLabelString: 'A, Area'
   };
 
   if ( phet.chipper.queryParameters.stringTest === 'xss' ) {
