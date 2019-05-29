e73ed5f04f842d0768af3a8c5aa58351ff810ebf
zepumph <michael.kauzmann@colorado.edu>
2019-05-29 15:51:56 -0800

    recompute baseline, https://github.com/phetsims/phet-io/issues/1466

--- js/phet-io/resistance-in-a-wire-phet-io-elements-baseline.js
+++ js/phet-io/resistance-in-a-wire-phet-io-elements-baseline.js
@@ -752,7 +752,7 @@ window.phet.phetio.phetioElementsBaseline =
     "resistanceInAWire.navigationBar.phetButton.pickableProperty": {
       "phetioDocumentation": "Set whether the phetButton will be pickable (and hence interactive), see the NodeIO documentation for more details",
       "phetioEventType": "model",
-      "phetioFeatured": true,
+      "phetioFeatured": false,
       "phetioHighFrequency": false,
       "phetioPlayback": false,
       "phetioReadOnly": false,
