3dea6fc91241899736a1b79a29b97b16d979a7e2
twant <taylor.want@gmail.com>
2019-10-13 20:11:29 -0700

    #210 move startDrag and endDrag back to top-level options.

--- js/resistance-in-a-wire/view/ControlPanel.js
+++ js/resistance-in-a-wire/view/ControlPanel.js
@@ -125,26 +125,26 @@ define( require => {
       StringUtils.format( pattern0ResistanceUnits1LengthUnitsString, symbolOhmsString, cmString ),
       resistivitySliderLabelString,
       tandem.createTandem( 'resistivitySlider' ), {
+        startDrag: function() {
+          rhoOnStart = model.resistivityProperty.get();
+          resistanceOnStart = model.resistanceProperty.get();
+        },
+        endDrag: function() {
+          const resistance = model.resistanceProperty.get();
+          const deltaRho = model.resistivityProperty.get() - rhoOnStart;
+          const deltaResistance = resistance - resistanceOnStart;
+
+          // announce to assistive technology if there is a change - no need to queue many alerts when pressing keys
+          // rapidly
+          if ( deltaRho && deltaResistance ) {
+            changeUtterance.alert = getSizeChangeAlert( resistance, deltaResistance, deltaRho, letterRhoString );
+            utteranceQueue.addToBack( changeUtterance );
+          }
+        },
         sliderOptions: {
           keyboardStep: 0.05, // ohm-cm
           shiftKeyStep: 0.01, // ohms-cm
           a11yCreateAriaValueText: value => StringUtils.fillIn( resistivityUnitsPatternString, { value: value } ),
-          startDrag: function() {
-            rhoOnStart = model.resistivityProperty.get();
-            resistanceOnStart = model.resistanceProperty.get();
-          },
-          endDrag: function() {
-            const resistance = model.resistanceProperty.get();
-            const deltaRho = model.resistivityProperty.get() - rhoOnStart;
-            const deltaResistance = resistance - resistanceOnStart;
-
-            // announce to assistive technology if there is a change - no need to queue many alerts when pressing keys
-            // rapidly
-            if ( deltaRho && deltaResistance ) {
-              changeUtterance.alert = getSizeChangeAlert( resistance, deltaResistance, deltaRho, letterRhoString );
-              utteranceQueue.addToBack( changeUtterance );
-            }
-          }
         }
       }
     );
@@ -159,26 +159,26 @@ define( require => {
       cmString,
       lengthSliderLabelString,
       tandem.createTandem( 'lengthSlider' ), {
+        startDrag: function() {
+          lengthOnStart = model.lengthProperty.get();
+          resistanceOnStart = model.resistanceProperty.get();
+        },
+        endDrag: function() {
+          const resistance = model.resistanceProperty.get();
+          const deltaLength = model.lengthProperty.get() - lengthOnStart;
+          const deltaResistance = resistance - resistanceOnStart;
+
+          // announce to assistive technology if there is a change - no need to queue many alerts when pressing keys
+          // rapidly
+          if ( deltaLength && deltaResistance ) {
+            changeUtterance.alert = getSizeChangeAlert( resistance, deltaResistance, deltaLength, letterLString );
+            utteranceQueue.addToBack( changeUtterance );
+          }
+        },
         sliderOptions: {
           keyboardStep: 1.0, // cm
           shiftKeyboardStep: 0.01, // cm
           a11yCreateAriaValueText: value => StringUtils.fillIn( lengthUnitsPatternString, { value: value } ),
-          startDrag: function() {
-            lengthOnStart = model.lengthProperty.get();
-            resistanceOnStart = model.resistanceProperty.get();
-          },
-          endDrag: function() {
-            const resistance = model.resistanceProperty.get();
-            const deltaLength = model.lengthProperty.get() - lengthOnStart;
-            const deltaResistance = resistance - resistanceOnStart;
-
-            // announce to assistive technology if there is a change - no need to queue many alerts when pressing keys
-            // rapidly
-            if ( deltaLength && deltaResistance ) {
-              changeUtterance.alert = getSizeChangeAlert( resistance, deltaResistance, deltaLength, letterLString );
-              utteranceQueue.addToBack( changeUtterance );
-            }
-          }
         }
       }
     );
@@ -194,26 +194,26 @@ define( require => {
       cmString + '<sup>2</sup>',
       areaSliderLabelString,
       tandem.createTandem( 'areaSlider' ), {
+        startDrag: function() {
+          areaOnStart = model.areaProperty.get();
+          resistanceOnStart = model.resistanceProperty.get();
+        },
+        endDrag: function() {
+          const resistance = model.resistanceProperty.get();
+          const deltaArea = model.areaProperty.get() - areaOnStart;
+          const deltaResistance = resistance - resistanceOnStart;
+
+          // announce to assistive technology if there is a change - no need to queue many alerts when pressing keys
+          // rapidly
+          if ( deltaArea && deltaResistance ) {
+            changeUtterance.alert = getSizeChangeAlert( resistance, deltaResistance, deltaArea, letterAString );
+            utteranceQueue.addToBack( changeUtterance );
+          }
+        },
         sliderOptions: {
           keyboardStep: 1.0, // cm^2
           shiftKeyboardStep: 0.01, // cm^2
           a11yCreateAriaValueText: value => StringUtils.fillIn( areaUnitsPatternString, { value: value } ),
-          startDrag: function() {
-            areaOnStart = model.areaProperty.get();
-            resistanceOnStart = model.resistanceProperty.get();
-          },
-          endDrag: function() {
-            const resistance = model.resistanceProperty.get();
-            const deltaArea = model.areaProperty.get() - areaOnStart;
-            const deltaResistance = resistance - resistanceOnStart;
-
-            // announce to assistive technology if there is a change - no need to queue many alerts when pressing keys
-            // rapidly
-            if ( deltaArea && deltaResistance ) {
-              changeUtterance.alert = getSizeChangeAlert( resistance, deltaResistance, deltaArea, letterAString );
-              utteranceQueue.addToBack( changeUtterance );
-            }
-          }
         }
       }
     );
