0c0f1a8e9b4d3be9a1eac6601936f258e4fb6406
samreid <reids@colorado.edu>
2020-02-27 12:07:57 -0700

    Add images and strings, see https://github.com/phetsims/chipper/issues/875

---
+++ js/resistance-in-a-wire-strings.js
@@ -0,0 +1,14 @@
+// Copyright 2020, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import resistanceInAWire from './resistanceInAWire.js';
+
+const resistanceInAWireStrings = getStringModule( 'RESISTANCE_IN_A_WIRE' );
+
+resistanceInAWire.register( 'resistanceInAWireStrings', resistanceInAWireStrings );
+
+export default resistanceInAWireStrings;
