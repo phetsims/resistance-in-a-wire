67dfd5d669f665802546a8fff7a92e696d946265
chrisklus <christopher.klusendorf@colorado.edu>
2020-03-30 14:45:36 -0400

    Update phet-io files for https://github.com/phetsims/joist/issues/597

--- js/phet-io/resistance-in-a-wire-phet-io-elements-baseline.js
+++ js/phet-io/resistance-in-a-wire-phet-io-elements-baseline.js
@@ -2003,6 +2003,19 @@ window.phet.phetio.phetioElementsBaseline = assert &&
       "phetioStudioControl": true,
       "phetioTypeName": "NumberPropertyIO"
     },
+    "resistanceInAWire.resistanceInAWireScreen.nameProperty": {
+      "phetioDocumentation": "",
+      "phetioDynamicElement": false,
+      "phetioEventType": "MODEL",
+      "phetioFeatured": false,
+      "phetioHighFrequency": false,
+      "phetioIsArchetype": false,
+      "phetioPlayback": false,
+      "phetioReadOnly": false,
+      "phetioState": true,
+      "phetioStudioControl": true,
+      "phetioTypeName": "PropertyIO<NullableIO<StringIO>>"
+    },
     "resistanceInAWire.resistanceInAWireScreen.view": {
       "phetioDocumentation": "",
       "phetioDynamicElement": false,
