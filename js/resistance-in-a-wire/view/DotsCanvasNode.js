0ff065bae5728c83e29b3d7de53c5ca828f3bb38
phet-dev <phetadmin@gmail.com>
2021-05-25 16:04:21 -0600

    update copyright dates from daily grunt work

--- js/resistance-in-a-wire/view/DotsCanvasNode.js
+++ js/resistance-in-a-wire/view/DotsCanvasNode.js
@@ -1,4 +1,4 @@
-// Copyright 2018-2020, University of Colorado Boulder
+// Copyright 2018-2021, University of Colorado Boulder
 
 /**
  * Draw the dots in the wire with a CanvasNode as a performance enhancement. This is much faster than drawing
