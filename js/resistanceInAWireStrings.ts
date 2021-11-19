3d87c90a2d082e7e7db361c60df3487976a185ea
Sam Reid <reids@colorado.edu>
2021-11-18 17:43:36 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/resistanceInAWireStrings.ts
@@ -0,0 +1,110 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import resistanceInAWire from './resistanceInAWire.js';
+
+type StringsType = {
+  'resistance-in-a-wire': {
+    'title': string
+  },
+  'area': string,
+  'areaSymbol': string,
+  'cm': string,
+  'length': string,
+  'lengthSymbol': string,
+  'ohm': string,
+  'pattern': {
+    '0label': {
+      '1value': {
+        '2units': string
+      }
+    },
+    '0resistanceUnits': {
+      '1lengthUnits': string
+    }
+  },
+  'resistance': string,
+  'resistanceSymbol': string,
+  'resistivity': string,
+  'a11y': {
+    'summary': {
+      'sim': string,
+      'currently': string,
+      'resistancePattern': string,
+      'resistivityPattern': string,
+      'lengthPattern': string,
+      'areaPattern': string,
+      'interactionHint': string
+    },
+    'equation': {
+      'resistanceEquation': string,
+      'resistanceEquationDescription': string,
+      'rhoLAndAComparablePattern': string,
+      'lAndAComparablePattern': string,
+      'noneComparablePattern': string,
+      'sizes': {
+        'muchMuchSmallerThan': string,
+        'muchSmallerThan': string,
+        'slightlySmallerThan': string,
+        'comparableTo': string,
+        'slightlyLargerThan': string,
+        'muchLargerThan': string,
+        'muchMuchLargerThan': string
+      },
+      'alerts': {
+        'grows': string,
+        'shrinks': string,
+        'growsALot': string,
+        'shrinksALot': string
+      }
+    },
+    'wire': {
+      'wireDescriptionPattern': string,
+      'resistivityUnitsPattern': string,
+      'extremelyShort': string,
+      'veryShort': string,
+      'short': string,
+      'ofMediumLength': string,
+      'long': string,
+      'veryLong': string,
+      'extremelyLong': string,
+      'extremelyThin': string,
+      'veryThin': string,
+      'thin': string,
+      'ofMediumThickness': string,
+      'thick': string,
+      'veryThick': string,
+      'extremelyThick': string,
+      'aTinyAmountOfImpurities': string,
+      'aVerySmallAmountOfImpurities': string,
+      'aSmallAmountOfImpurities': string,
+      'aMediumAmountOfImpurities': string,
+      'aLargeAmountOfImpurities': string,
+      'aVeryLargeAmountOfImpurities': string,
+      'aHugeAmountOfImpurities': string
+    },
+    'controls': {
+      'lengthUnitsPattern': string,
+      'areaUnitsPattern': string,
+      'resistivitySliderLabel': string,
+      'lengthSliderLabel': string,
+      'areaSliderLabel': string,
+      'sliderControls': string,
+      'slidersDescription': string,
+      'sizeChangeAlertPattern': string,
+      'letterRho': string,
+      'letterL': string,
+      'letterA': string
+    }
+  }
+};
+
+const resistanceInAWireStrings = getStringModule( 'RESISTANCE_IN_A_WIRE' ) as StringsType;
+
+resistanceInAWire.register( 'resistanceInAWireStrings', resistanceInAWireStrings );
+
+export default resistanceInAWireStrings;
