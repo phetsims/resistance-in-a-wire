8ed0bfac8935775128bd49f43a2b92ad09269373
Jesse <jesse.greenberg@colorado.edu>
2021-12-16 18:49:57 -0500

    SliderAndGeneralKeyboardHelpContent.js -> SliderControlsAndBasicActionsKeyboardHelpContent.js, see https://github.com/phetsims/scenery-phet/issues/714

--- js/resistance-in-a-wire/ResistanceInAWireScreen.js
+++ js/resistance-in-a-wire/ResistanceInAWireScreen.js
@@ -9,7 +9,7 @@
 
 import Property from '../../../axon/js/Property.js';
 import Screen from '../../../joist/js/Screen.js';
-import SliderAndGeneralKeyboardHelpContent from '../../../scenery-phet/js/keyboard/help/SliderAndGeneralKeyboardHelpContent.js';
+import SliderControlsAndBasicActionsKeyboardHelpContent from '../../../scenery-phet/js/keyboard/help/SliderControlsAndBasicActionsKeyboardHelpContent.js';
 import resistanceInAWire from '../resistanceInAWire.js';
 import ResistanceInAWireModel from './model/ResistanceInAWireModel.js';
 import ResistanceInAWireScreenView from './view/ResistanceInAWireScreenView.js';
@@ -25,7 +25,7 @@ class ResistanceInAWireScreen extends Screen {
       model => new ResistanceInAWireScreenView( model, tandem.createTandem( 'view' ) ), {
         backgroundColorProperty: new Property( '#ffffdf' ),
         tandem: tandem,
-        keyboardHelpNode: new SliderAndGeneralKeyboardHelpContent()
+        keyboardHelpNode: new SliderControlsAndBasicActionsKeyboardHelpContent()
       }
     );
   }
