8ae59c68f802fbdbf7bcc0cd3aef9179aeeb36a0
phet-dev <phetadmin@gmail.com>
2021-12-17 03:24:09 -0700

    update copyright dates from daily grunt work

--- js/resistance-in-a-wire/ResistanceInAWireScreen.js
+++ js/resistance-in-a-wire/ResistanceInAWireScreen.js
@@ -1,4 +1,4 @@
-// Copyright 2016-2020, University of Colorado Boulder
+// Copyright 2016-2021, University of Colorado Boulder
 
 /**
  * The main screen class for the 'Resistance in a Wire' simulation.  This is where the main model and view instances are
