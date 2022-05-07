0beedca3cde30782643a836d366ad2a135a4075c
Sam Reid <reids@colorado.edu>
2022-05-07 09:34:11 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/resistanceInAWireStrings.ts
+++ js/resistanceInAWireStrings.ts
@@ -9,96 +9,96 @@ import resistanceInAWire from './resistanceInAWire.js';
 
 type StringsType = {
   'resistance-in-a-wire': {
-    'title': string
-  },
-  'area': string,
-  'areaSymbol': string,
-  'cm': string,
-  'length': string,
-  'lengthSymbol': string,
-  'ohm': string,
+    'title': string;
+  };
+  'area': string;
+  'areaSymbol': string;
+  'cm': string;
+  'length': string;
+  'lengthSymbol': string;
+  'ohm': string;
   'pattern': {
     '0label': {
       '1value': {
-        '2units': string
+        '2units': string;
       }
-    },
+    };
     '0resistanceUnits': {
-      '1lengthUnits': string
+      '1lengthUnits': string;
     }
-  },
-  'resistance': string,
-  'resistanceSymbol': string,
-  'resistivity': string,
+  };
+  'resistance': string;
+  'resistanceSymbol': string;
+  'resistivity': string;
   'a11y': {
     'summary': {
-      'sim': string,
-      'currently': string,
-      'resistancePattern': string,
-      'resistivityPattern': string,
-      'lengthPattern': string,
-      'areaPattern': string,
-      'interactionHint': string
-    },
+      'sim': string;
+      'currently': string;
+      'resistancePattern': string;
+      'resistivityPattern': string;
+      'lengthPattern': string;
+      'areaPattern': string;
+      'interactionHint': string;
+    };
     'equation': {
-      'resistanceEquation': string,
-      'resistanceEquationDescription': string,
-      'rhoLAndAComparablePattern': string,
-      'lAndAComparablePattern': string,
-      'noneComparablePattern': string,
+      'resistanceEquation': string;
+      'resistanceEquationDescription': string;
+      'rhoLAndAComparablePattern': string;
+      'lAndAComparablePattern': string;
+      'noneComparablePattern': string;
       'sizes': {
-        'muchMuchSmallerThan': string,
-        'muchSmallerThan': string,
-        'slightlySmallerThan': string,
-        'comparableTo': string,
-        'slightlyLargerThan': string,
-        'muchLargerThan': string,
-        'muchMuchLargerThan': string
-      },
+        'muchMuchSmallerThan': string;
+        'muchSmallerThan': string;
+        'slightlySmallerThan': string;
+        'comparableTo': string;
+        'slightlyLargerThan': string;
+        'muchLargerThan': string;
+        'muchMuchLargerThan': string;
+      };
       'alerts': {
-        'grows': string,
-        'shrinks': string,
-        'growsALot': string,
-        'shrinksALot': string
+        'grows': string;
+        'shrinks': string;
+        'growsALot': string;
+        'shrinksALot': string;
       }
-    },
+    };
     'wire': {
-      'wireDescriptionPattern': string,
-      'resistivityUnitsPattern': string,
-      'extremelyShort': string,
-      'veryShort': string,
-      'short': string,
-      'ofMediumLength': string,
-      'long': string,
-      'veryLong': string,
-      'extremelyLong': string,
-      'extremelyThin': string,
-      'veryThin': string,
-      'thin': string,
-      'ofMediumThickness': string,
-      'thick': string,
-      'veryThick': string,
-      'extremelyThick': string,
-      'aTinyAmountOfImpurities': string,
-      'aVerySmallAmountOfImpurities': string,
-      'aSmallAmountOfImpurities': string,
-      'aMediumAmountOfImpurities': string,
-      'aLargeAmountOfImpurities': string,
-      'aVeryLargeAmountOfImpurities': string,
-      'aHugeAmountOfImpurities': string
-    },
+      'wireDescriptionPattern': string;
+      'resistivityUnitsPattern': string;
+      'extremelyShort': string;
+      'veryShort': string;
+      'short': string;
+      'ofMediumLength': string;
+      'long': string;
+      'veryLong': string;
+      'extremelyLong': string;
+      'extremelyThin': string;
+      'veryThin': string;
+      'thin': string;
+      'ofMediumThickness': string;
+      'thick': string;
+      'veryThick': string;
+      'extremelyThick': string;
+      'aTinyAmountOfImpurities': string;
+      'aVerySmallAmountOfImpurities': string;
+      'aSmallAmountOfImpurities': string;
+      'aMediumAmountOfImpurities': string;
+      'aLargeAmountOfImpurities': string;
+      'aVeryLargeAmountOfImpurities': string;
+      'aHugeAmountOfImpurities': string;
+    };
     'controls': {
-      'lengthUnitsPattern': string,
-      'areaUnitsPattern': string,
-      'resistivitySliderLabel': string,
-      'lengthSliderLabel': string,
-      'areaSliderLabel': string,
-      'sliderControls': string,
-      'slidersDescription': string,
-      'sizeChangeAlertPattern': string,
-      'letterRho': string,
-      'letterL': string,
-      'letterA': string
+      'lengthUnitsPattern': string;
+      'areaUnitsPattern': string;
+      'resistivitySliderLabel': string;
+      'lengthSliderLabel': string;
+      'areaSliderLabel': string;
+      'sliderControls': string;
+      'slidersDescription': string;
+      'sizeChangeAlertPattern': string;
+      'letterRho': string;
+      'letterL': string;
+      'letterA': string;
     }
   }
 };
