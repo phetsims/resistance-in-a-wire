513317081b1b9229b9746cf1ccdb112d3d920b72
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:35 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/resistanceInAWireStrings.ts
+++ js/resistanceInAWireStrings.ts
@@ -5,100 +5,170 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import resistanceInAWire from './resistanceInAWire.js';
 
 type StringsType = {
   'resistance-in-a-wire': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'area': string;
+  'areaProperty': TReadOnlyProperty<string>;
   'areaSymbol': string;
+  'areaSymbolProperty': TReadOnlyProperty<string>;
   'cm': string;
+  'cmProperty': TReadOnlyProperty<string>;
   'length': string;
+  'lengthProperty': TReadOnlyProperty<string>;
   'lengthSymbol': string;
+  'lengthSymbolProperty': TReadOnlyProperty<string>;
   'ohm': string;
+  'ohmProperty': TReadOnlyProperty<string>;
   'pattern': {
     '0label': {
       '1value': {
         '2units': string;
+        '2unitsProperty': TReadOnlyProperty<string>;
       }
     };
     '0resistanceUnits': {
       '1lengthUnits': string;
+      '1lengthUnitsProperty': TReadOnlyProperty<string>;
     }
   };
   'resistance': string;
+  'resistanceProperty': TReadOnlyProperty<string>;
   'resistanceSymbol': string;
+  'resistanceSymbolProperty': TReadOnlyProperty<string>;
   'resistivity': string;
+  'resistivityProperty': TReadOnlyProperty<string>;
   'a11y': {
     'summary': {
       'sim': string;
+      'simProperty': TReadOnlyProperty<string>;
       'currently': string;
+      'currentlyProperty': TReadOnlyProperty<string>;
       'resistancePattern': string;
+      'resistancePatternProperty': TReadOnlyProperty<string>;
       'resistivityPattern': string;
+      'resistivityPatternProperty': TReadOnlyProperty<string>;
       'lengthPattern': string;
+      'lengthPatternProperty': TReadOnlyProperty<string>;
       'areaPattern': string;
+      'areaPatternProperty': TReadOnlyProperty<string>;
       'interactionHint': string;
+      'interactionHintProperty': TReadOnlyProperty<string>;
     };
     'equation': {
       'resistanceEquation': string;
+      'resistanceEquationProperty': TReadOnlyProperty<string>;
       'resistanceEquationDescription': string;
+      'resistanceEquationDescriptionProperty': TReadOnlyProperty<string>;
       'rhoLAndAComparablePattern': string;
+      'rhoLAndAComparablePatternProperty': TReadOnlyProperty<string>;
       'lAndAComparablePattern': string;
+      'lAndAComparablePatternProperty': TReadOnlyProperty<string>;
       'noneComparablePattern': string;
+      'noneComparablePatternProperty': TReadOnlyProperty<string>;
       'sizes': {
         'muchMuchSmallerThan': string;
+        'muchMuchSmallerThanProperty': TReadOnlyProperty<string>;
         'muchSmallerThan': string;
+        'muchSmallerThanProperty': TReadOnlyProperty<string>;
         'slightlySmallerThan': string;
+        'slightlySmallerThanProperty': TReadOnlyProperty<string>;
         'comparableTo': string;
+        'comparableToProperty': TReadOnlyProperty<string>;
         'slightlyLargerThan': string;
+        'slightlyLargerThanProperty': TReadOnlyProperty<string>;
         'muchLargerThan': string;
+        'muchLargerThanProperty': TReadOnlyProperty<string>;
         'muchMuchLargerThan': string;
+        'muchMuchLargerThanProperty': TReadOnlyProperty<string>;
       };
       'alerts': {
         'grows': string;
+        'growsProperty': TReadOnlyProperty<string>;
         'shrinks': string;
+        'shrinksProperty': TReadOnlyProperty<string>;
         'growsALot': string;
+        'growsALotProperty': TReadOnlyProperty<string>;
         'shrinksALot': string;
+        'shrinksALotProperty': TReadOnlyProperty<string>;
       }
     };
     'wire': {
       'wireDescriptionPattern': string;
+      'wireDescriptionPatternProperty': TReadOnlyProperty<string>;
       'resistivityUnitsPattern': string;
+      'resistivityUnitsPatternProperty': TReadOnlyProperty<string>;
       'extremelyShort': string;
+      'extremelyShortProperty': TReadOnlyProperty<string>;
       'veryShort': string;
+      'veryShortProperty': TReadOnlyProperty<string>;
       'short': string;
+      'shortProperty': TReadOnlyProperty<string>;
       'ofMediumLength': string;
+      'ofMediumLengthProperty': TReadOnlyProperty<string>;
       'long': string;
+      'longProperty': TReadOnlyProperty<string>;
       'veryLong': string;
+      'veryLongProperty': TReadOnlyProperty<string>;
       'extremelyLong': string;
+      'extremelyLongProperty': TReadOnlyProperty<string>;
       'extremelyThin': string;
+      'extremelyThinProperty': TReadOnlyProperty<string>;
       'veryThin': string;
+      'veryThinProperty': TReadOnlyProperty<string>;
       'thin': string;
+      'thinProperty': TReadOnlyProperty<string>;
       'ofMediumThickness': string;
+      'ofMediumThicknessProperty': TReadOnlyProperty<string>;
       'thick': string;
+      'thickProperty': TReadOnlyProperty<string>;
       'veryThick': string;
+      'veryThickProperty': TReadOnlyProperty<string>;
       'extremelyThick': string;
+      'extremelyThickProperty': TReadOnlyProperty<string>;
       'aTinyAmountOfImpurities': string;
+      'aTinyAmountOfImpuritiesProperty': TReadOnlyProperty<string>;
       'aVerySmallAmountOfImpurities': string;
+      'aVerySmallAmountOfImpuritiesProperty': TReadOnlyProperty<string>;
       'aSmallAmountOfImpurities': string;
+      'aSmallAmountOfImpuritiesProperty': TReadOnlyProperty<string>;
       'aMediumAmountOfImpurities': string;
+      'aMediumAmountOfImpuritiesProperty': TReadOnlyProperty<string>;
       'aLargeAmountOfImpurities': string;
+      'aLargeAmountOfImpuritiesProperty': TReadOnlyProperty<string>;
       'aVeryLargeAmountOfImpurities': string;
+      'aVeryLargeAmountOfImpuritiesProperty': TReadOnlyProperty<string>;
       'aHugeAmountOfImpurities': string;
+      'aHugeAmountOfImpuritiesProperty': TReadOnlyProperty<string>;
     };
     'controls': {
       'lengthUnitsPattern': string;
+      'lengthUnitsPatternProperty': TReadOnlyProperty<string>;
       'areaUnitsPattern': string;
+      'areaUnitsPatternProperty': TReadOnlyProperty<string>;
       'resistivitySliderLabel': string;
+      'resistivitySliderLabelProperty': TReadOnlyProperty<string>;
       'lengthSliderLabel': string;
+      'lengthSliderLabelProperty': TReadOnlyProperty<string>;
       'areaSliderLabel': string;
+      'areaSliderLabelProperty': TReadOnlyProperty<string>;
       'sliderControls': string;
+      'sliderControlsProperty': TReadOnlyProperty<string>;
       'slidersDescription': string;
+      'slidersDescriptionProperty': TReadOnlyProperty<string>;
       'sizeChangeAlertPattern': string;
+      'sizeChangeAlertPatternProperty': TReadOnlyProperty<string>;
       'letterRho': string;
+      'letterRhoProperty': TReadOnlyProperty<string>;
       'letterL': string;
+      'letterLProperty': TReadOnlyProperty<string>;
       'letterA': string;
+      'letterAProperty': TReadOnlyProperty<string>;
     }
   }
 };
