264a3da0cbf9576ca35b0b343fc1b9386415bc56
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-09-26 14:09:35 -0600

    typo, https://github.com/phetsims/chipper/issues/1474
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 // Copyright 2024, University of Colorado Boulder
 
 /**
- * ESlint configuration for resistance-in-a-wire.
+ * ESLint configuration for resistance-in-a-wire.
  *
  * @author Sam Reid (PhET Interactive Simulations)
  * @author Michael Kauzmann (PhET Interactive Simulations)
