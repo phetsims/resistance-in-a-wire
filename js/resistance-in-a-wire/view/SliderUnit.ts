97297e9aa483c09f3ee08374bb33a789d1f7f590
Jesse <jesse.greenberg@colorado.edu>
2026-05-13 10:55:21 -0400

    sync-to-poly: ts conversion, see https://github.com/phetsims/resistance-in-a-wire/issues/222
    Mono-SHA: 62e0701314483d7de99afe54837a5b395ae1dbf8

--- js/resistance-in-a-wire/view/SliderUnit.ts
+++ js/resistance-in-a-wire/view/SliderUnit.ts
@@ -1,6 +1,4 @@
 // Copyright 2017-2026, University of Colorado Boulder
-/* eslint-disable */
-// @ts-nocheck
 
 /**
  * Slider unit with a vertical slider, a title above the slider and a readout display below the slider. Layout is dynamic
@@ -8,31 +6,46 @@
  * @author Martin Veillette (Berea College)
  */
 
+import type PhetioProperty from '../../../../axon/js/PhetioProperty.js';
+import type { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
+import type RangeWithValue from '../../../../dot/js/RangeWithValue.js';
 import Utils from '../../../../dot/js/Utils.js';
-import merge from '../../../../phet-core/js/merge.js';
-import Node from '../../../../scenery/js/nodes/Node.js';
+import optionize from '../../../../phet-core/js/optionize.js';
+import type SceneryEvent from '../../../../scenery/js/input/SceneryEvent.js';
+import Node, { type NodeOptions } from '../../../../scenery/js/nodes/Node.js';
 import RichText from '../../../../scenery/js/nodes/RichText.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
-import VSlider from '../../../../sun/js/VSlider.js';
+import VSlider, { type VSliderOptions } from '../../../../sun/js/VSlider.js';
+import type Tandem from '../../../../tandem/js/Tandem.js';
 import ResistanceInAWireConstants from '../ResistanceInAWireConstants.js';
 
-class SliderUnit extends Node {
-  /**
-   * @param {Property.<number>} property
-   * @param {RangeWithValue} range
-   * @param {string} symbolString
-   * @param {string} nameString
-   * @param {string} unitString
-   * @param {string} labelContent - a11y, label read by a screen reader
-   * @param {Tandem} tandem
-   * @param {Object} [options]
-   */
-  constructor( property, range, symbolString, nameString, unitString, labelContent, tandem, options ) {
+type SelfOptions = {
+  sliderOptions?: VSliderOptions;
+  startDrag?: () => void;
+  endDrag?: () => void;
+  decimalPlaces?: number;
+};
+
+type SliderUnitOptions = SelfOptions & NodeOptions;
+
+export default class SliderUnit extends Node {
+
+  // Whether the slider is currently being dragged by the keyboard. Used by the sound generator.
+  public keyboardDragging: boolean;
+
+  public constructor( property: PhetioProperty<number>,
+                      range: RangeWithValue,
+                      symbolString: string | TReadOnlyProperty<string>,
+                      nameString: string,
+                      unitString: string,
+                      labelContent: string,
+                      tandem: Tandem,
+                      providedOptions?: SliderUnitOptions ) {
 
     super();
 
-    options = merge( {
+    const options = optionize<SliderUnitOptions, SelfOptions, NodeOptions>()( {
       sliderOptions: {
         trackFillEnabled: 'black',
         trackSize: new Dimension2( 4, ResistanceInAWireConstants.SLIDER_HEIGHT - 30 ),
@@ -41,7 +54,7 @@ class SliderUnit extends Node {
         thumbFillHighlighted: '#dedede',
 
         // physical values in this sim can have up to 2 decimals
-        constrainValue: value => Utils.toFixedNumber( value, 2 ),
+        constrainValue: ( value: number ) => Utils.toFixedNumber( value, 2 ),
         startDrag: _.noop,
         endDrag: _.noop,
 
@@ -55,20 +68,22 @@ class SliderUnit extends Node {
         containerTagName: 'li',
         labelContent: labelContent,
         labelTagName: 'label',
-        mapPDOMValue: value => Utils.toFixedNumber( value, 2 ),
+        mapPDOMValue: ( value: number ) => Utils.toFixedNumber( value, 2 ),
 
         // phet-io
         tandem: tandem.createTandem( 'slider' )
       },
 
       // {number}
-      decimalPlaces: 0
+      decimalPlaces: 0,
 
-    }, options );
+      startDrag: _.noop,
+      endDrag: _.noop
+    }, providedOptions );
 
-    // override the start and end drag functions in the options
+    // override the start and end drag functions in the slider options
     const providedStartDragFunction = options.startDrag;
-    options.sliderOptions.startDrag = event => {
+    options.sliderOptions.startDrag = ( event: SceneryEvent ) => {
       if ( event.type === 'keydown' ) {
         this.keyboardDragging = true;
       }
@@ -96,10 +111,8 @@ class SliderUnit extends Node {
       tandem: tandem.createTandem( 'nameText' )
     } );
 
-    // @public (read-only) {boolean} - flag that indicates whether the slider is being dragged by the keyboard
     this.keyboardDragging = false;
 
-    // @private
     const slider = new VSlider( property, range, options.sliderOptions );
 
     const valueText = new Text( Utils.toFixed( range.max, 2 ), {
@@ -144,7 +157,7 @@ class SliderUnit extends Node {
     this.addChild( unitText );
 
     // Update value of the readout. No need to unlink, present for the lifetime of the simulation.
-    property.link( value => {
+    property.link( ( value: number ) => {
       valueText.string = Utils.toFixed( value, 2 );
       valueText.centerX = unitText.centerX;
     } );
@@ -152,5 +165,3 @@ class SliderUnit extends Node {
     this.mutate( options );
   }
 }
-
-export default SliderUnit;
