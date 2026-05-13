5521e25a94a197ced219f7e91759064ae77858bf
Jesse <jesse.greenberg@colorado.edu>
2026-05-13 09:44:58 -0400

    sync-to-poly: typescript port, see https://github.com/phetsims/resistance-in-a-wire/issues/222
    Mono-SHA: dff6019b559f61ffe43744f55b05ef303d957343

--- js/resistance-in-a-wire/model/ResistanceInAWireModel.ts
+++ js/resistance-in-a-wire/model/ResistanceInAWireModel.ts
@@ -9,39 +9,47 @@
 
 import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import type { TReadOnlyProperty } from '../../../../axon/js/TReadOnlyProperty.js';
 import Range from '../../../../dot/js/Range.js';
+import TModel from '../../../../joist/js/TModel.js';
+import Tandem from '../../../../tandem/js/Tandem.js';
 import NumberIO from '../../../../tandem/js/types/NumberIO.js';
 import ResistanceInAWireConstants from '../ResistanceInAWireConstants.js';
 
-class ResistanceInAWireModel {
-  /**
-   * @param {Tandem} tandem
-   */
-  constructor( tandem ) {
+export default class ResistanceInAWireModel implements TModel {
 
-    // @public {Property.<number>} in Ohm*cm
+  // Resistivity of the wire, in Ohm-centimeters.
+  public readonly resistivityProperty: NumberProperty;
+
+  // Length of the wire, in centimeters.
+  public readonly lengthProperty: NumberProperty;
+
+  // Cross-sectional area of the wire, in square centimeters.
+  public readonly areaProperty: NumberProperty;
+
+  // Resistance of the wire, derived from resistivity, length, and area, in Ohms.
+  public readonly resistanceProperty: TReadOnlyProperty<number>;
+
+  public constructor( tandem: Tandem ) {
+
     this.resistivityProperty = new NumberProperty( ResistanceInAWireConstants.RESISTIVITY_RANGE.defaultValue, {
       tandem: tandem.createTandem( 'resistivityProperty' ),
       units: '\u03A9\u00b7cm', // Ohm-centimeters
       range: ResistanceInAWireConstants.RESISTIVITY_RANGE
     } );
 
-    // @public {Property.<number>} in cm
     this.lengthProperty = new NumberProperty( ResistanceInAWireConstants.LENGTH_RANGE.defaultValue, {
       tandem: tandem.createTandem( 'lengthProperty' ),
       units: 'cm',
       range: ResistanceInAWireConstants.LENGTH_RANGE
     } );
 
-    // @public {Property.<number>} in cm^2
     this.areaProperty = new NumberProperty( ResistanceInAWireConstants.AREA_RANGE.defaultValue, {
       tandem: tandem.createTandem( 'areaProperty' ),
       units: 'cm^2',
       range: ResistanceInAWireConstants.AREA_RANGE
     } );
 
-    // Derived property that tracks the resistance of the wire
-    // @public {Property.<number>} in Ohms
     this.resistanceProperty = new DerivedProperty( [ this.resistivityProperty, this.lengthProperty, this.areaProperty ],
       ( resistivity, length, area ) => resistivity * length / area, {
         tandem: tandem.createTandem( 'resistanceProperty' ),
@@ -53,26 +61,18 @@ class ResistanceInAWireModel {
 
 
   /**
-   * resets the properties of the model
-   * @public
+   * Resets the independent properties of the model.
    */
-  reset() {
+  public reset(): void {
     this.resistivityProperty.reset();
     this.lengthProperty.reset();
     this.areaProperty.reset();
   }
 
   /**
-   * Get the total range of the derived resistance from the independent Properties of this model.
-   *
-   * @returns {Range}
-   * @public
+   * Returns the total range of resistance values supported by the model's independent properties.
    */
-  static getResistanceRange() {
-    const minResistance = ResistanceInAWireConstants.RESISTIVITY_RANGE.min * ResistanceInAWireConstants.LENGTH_RANGE.min / ResistanceInAWireConstants.AREA_RANGE.min;
-    const maxResistance = ResistanceInAWireConstants.RESISTIVITY_RANGE.max * ResistanceInAWireConstants.LENGTH_RANGE.max / ResistanceInAWireConstants.AREA_RANGE.max;
-    return new Range( minResistance, maxResistance );
+  public static getResistanceRange(): Range {
+    return ResistanceInAWireConstants.RESISTANCE_RANGE;
   }
 }
-
-export default ResistanceInAWireModel;
